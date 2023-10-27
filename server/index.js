af71b470c20f67efcfa1c58621a0c4ecf1229563
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-27 19:48:09 +0530

    Initial setup and required package install

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+import cors from 'cors';
+import testRouter from './routes/test.route.js'
+
+dotenv.config(); // if .env file is not in root folder then we have to do something like this dotenv.config({path:'./config/'});
+
+const app = express(); //All the functionality of express is now store in app variable
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev')); //Morgan is used to hit the url on console if we hit api
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`.bgBlue.white)
+});
+
+app.use('/api/v1', testRouter);
