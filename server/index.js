545fd18b95320a74a03ac206ce8993c3323a21f7
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-27 20:13:38 +0530

    connected mongodb with server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
 import express from "express";
-import dotenv from 'dotenv';
-import colors from 'colors';
-import morgan from 'morgan';
-import cors from 'cors';
-import testRouter from './routes/test.route.js'
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import cors from "cors";
+import testRouter from "./routes/test.route.js";
+import connectDB from "./config/db.js";
 
 dotenv.config(); // if .env file is not in root folder then we have to do something like this dotenv.config({path:'./config/'});
+
+connectDB();
 
 const app = express(); //All the functionality of express is now store in app variable
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev')); //Morgan is used to hit the url on console if we hit api
+app.use(morgan("dev")); //Morgan is used to hit the url on console if we hit api
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`.bgBlue.white)
+  console.log(`server is running on port ${PORT}`.bgBlue.white);
 });
 
-app.use('/api/v1', testRouter);
+app.use("/api/v1", testRouter);
